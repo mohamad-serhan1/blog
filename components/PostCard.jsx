d1462656f7673f0b89003a129d600f5f3bd0980b
mohamad-serhan1 <mohd.ser19@gmail.com>
2023-12-17 22:10:02 +0200

    first commit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -43,6 +43,7 @@ const PostCard = ({ post }) => {
                 <Link to={`/post/${post.slug}`}>
                     <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
                 </Link>
+                //kj
             </div>
         </div>
     )
